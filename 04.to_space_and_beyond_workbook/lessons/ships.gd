31db2c56e7a5f8ed6f7206a828fb6b70450f6b44
Tonykuang1028 <jikuang1@s.sfusd.edu>
2024-03-15 15:25:25 -0700

    L3: taking control

--- 04.to_space_and_beyond_workbook/lessons/ships.gd
+++ 04.to_space_and_beyond_workbook/lessons/ships.gd
@@ -1,13 +1,18 @@
 extends Sprite2D
+var max_speed := 600.0
+var velocity := Vector2(0, 0)
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-var velocity := Vector2(500, 0)
 
 func _process(delta: float) -> void:
+	var direction := Vector2(0, 0)
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	if direction.length() > 1.0: 
+		direction = direction.normalized()
+
+	velocity = direction * max_speed
 	position += velocity * delta
-	rotation = velocity.angle()
+	if direction.length() > 0.0:
+		rotation = velocity.angle()
+	
+	
